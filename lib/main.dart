a91d94b23cd81851c46043cc08547f37dad11767
chlorineCtrl <abirhasan5@iut-dhaka.edu>
2023-07-23 20:23:19 +0600

    CREATE-with static data

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_fire_crud/firebase_options.dart';
@@ -38,17 +39,39 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final controller = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
           appBar: AppBar(
-            title: TextField(),
+            title: TextField(
+              controller: controller,
+            ),
             actions: [
-              IconButton(onPressed: () {}, icon: const Icon(Icons.add),)
+              IconButton(
+                onPressed: () {
+                final name = controller.text;
 
+                createUser(name: name);
+              },
+                icon: const Icon(Icons.add),
+              ),
             ],
           ),
     );
+  }
+
+  Future createUser({required String name}) async {
+    final docUser = FirebaseFirestore.instance.collection('users').doc('my-id2');
+
+    final json = {
+      'name': name,
+      'age': 21,
+      'birthday': DateTime(2023,6,29),
+    };
+
+    await docUser.set(json);
+
   }
 }
